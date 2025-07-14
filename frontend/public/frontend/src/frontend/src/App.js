3bfe0a413b8c319ac130f793036ef53898d076c3
codigorebook <golden.cliente@gmail.com>
2025-07-14 06:10:50 -0400

    Update App.js

--- frontend/public/frontend/src/frontend/src/App.js
+++ frontend/public/frontend/src/frontend/src/App.js
@@ -1,1 +1,16 @@
-frontend/src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Codigo R</h1>
+        <p>Ebook de Trading de Criptomoedas</p>
+        <p>Site em construção...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
