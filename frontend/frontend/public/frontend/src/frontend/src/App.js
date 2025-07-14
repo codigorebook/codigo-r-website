a9bcb1a4fe550de3f57a133761dcb1006313aa4f
codigorebook <golden.cliente@gmail.com>
2025-07-14 06:38:42 -0400

    Create App.js

---
+++ frontend/frontend/public/frontend/src/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Codigo R</h1>
+        <p>Ebook de Trading de Criptomoedas</p>
+        <p>Site em construção...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
